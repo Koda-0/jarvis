8852aab0506210a0039bf1ce4a68777d0c14d718
ishimwengambasabri@gmail.com <ishimwengambasabri@gmail.com>
2026-07-14 14:34:19 +0200

    set cors to frontend url

--- Data/jarvis/backend/app.js
+++ Data/jarvis/backend/app.js
@@ -20,6 +20,7 @@ app.use(cors({
         'http://localhost:5175',
         'http://127.0.0.1:5173',
         'http://127.0.0.1:5174',
+        'https://ishimwesabri-tau.vercel.app'
     ],
     credentials: true,
 }));
