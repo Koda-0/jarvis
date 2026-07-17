ee38e9777805b0a0f7ccebc5524a62120abd4199
ishimwengambasabri@gmail.com <ishimwengambasabri@gmail.com>
2026-07-17 13:09:31 +0200

    new changes

--- Data/jarvis/backend/controller/mail.controller.js
+++ Data/jarvis/backend/controller/mail.controller.js
@@ -7,8 +7,8 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_APP_PASSWORD,
+        user: 'ishimwengambasabri@gmail.com',
+        pass: 'nuic ycdh nyxp gkna',
     },
 });
 
